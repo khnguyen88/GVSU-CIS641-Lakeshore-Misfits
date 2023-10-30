d8a6241ba9f40e721f9e2464cd8df3da4501074a
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-10-29 20:50:06 -0400

    Restructured service files to present more like JS classes instead of ReactJS component classes. Added a generic randomColorGeneratorService.

--- src/website-color-generator-app/src/services/ContrastCheckerAPIService/ContrastCheckerAPIService.js
+++ src/website-color-generator-app/src/services/ContrastCheckerAPIService/ContrastCheckerAPIService.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-
-const ContrastCheckerApiService = () => (
-  <div data-testid="ContrastCheckerApiService">
-    ContrastCheckerApiService Component
-  </div>
-);
-
-ContrastCheckerApiService.propTypes = {};
-
-ContrastCheckerApiService.defaultProps = {};
-
-export default ContrastCheckerApiService;
+export default class ContrastCheckerApiService { }
