1ae77eccec3ba07d6bddd407983ac126be3fea26
selenacade <cades@mail.gvsu.edu>
2023-10-31 01:43:29 -0400

    changes

--- src/website-color-generator-app/src/pages/AboutUs/AboutUs.js
+++ src/website-color-generator-app/src/pages/AboutUs/AboutUs.js
@@ -10,7 +10,7 @@ const AboutUs = () => (
       <p>Our website is created for users to generate new colors with a click of a button. This website allows users to generate new color palettes and copy the color values. Users are also able to adjust the color palette to make sure the colors sets is compliant with the AA or AAA website accessibility. In order to have users check this, we have a contrast checker button that tells users if the colors passess or fails the web accessibility guidelines.</p>
       <h3>Project Inspiration</h3>
       <p>This project was inspired by each members interests. We are interested in Web Content Acessibiility Guideline, art, science,application design, and illustration. </p>
-      <h3>Our Individual Roles </h3> 
+      <h3>Individual Roles </h3> 
       <ul>
         <li>Selena: project schedules, mock site layout, contrast checker page layout, and about page layout.</li>
         <li>Khiem: requirments and task refining, initial app setup, navigation routes, and integration</li>
