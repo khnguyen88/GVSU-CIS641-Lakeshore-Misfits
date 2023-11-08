3d948de0f3151da865b10d9eb9cf000090e2f996
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-11-08 00:16:24 -0500

    Populated ContrastCheckerAPISerivce class and it's GetColorPairContrastRatings() method.

--- src/website-color-generator-app/src/services/ContrastCheckerAPIService/ContrastCheckerAPIService.js
+++ src/website-color-generator-app/src/services/ContrastCheckerAPIService/ContrastCheckerAPIService.js
@@ -1,1 +1,25 @@
-export default class ContrastCheckerApiService { }
+import axios from 'axios';
+
+export default class ContrastCheckerApiService { 
+
+    constructor() { }
+    
+    async GetColorPairContrastRatings(fgColor, bgColor) {
+        try {
+            const res = await axios
+                .get(
+                    `https://webaim.org/resources/contrastchecker/?fcolor=${fgColor}&bcolor=${bgColor}&api`
+                );
+            
+            const itemJSON = res.data;
+
+            console.log(JSON.stringify(itemJSON));
+            
+            return itemJSON;
+        }
+        catch(err) {
+            console.log(err);
+            alert(JSON.stringify(err));
+        }
+    }
+}
