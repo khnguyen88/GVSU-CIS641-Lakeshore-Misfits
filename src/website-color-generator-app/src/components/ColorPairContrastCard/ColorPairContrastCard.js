dfab5e473d26eb88ab83faeba760391aa3505c39
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-11-18 15:17:37 -0500

    Updated card component to display color pair's contrast ratings and colors. Updated contrast checker page to generate cards from Map Array

--- src/website-color-generator-app/src/components/ColorPairContrastCard/ColorPairContrastCard.js
+++ src/website-color-generator-app/src/components/ColorPairContrastCard/ColorPairContrastCard.js
@@ -1,103 +1,31 @@
 import React, { useContext } from 'react';
 import './ColorPairContrastCard.css'
-import { PaletteContext } from '../../context/PaletteContext';
 
-function ColorPairContrastCard(body) {
-    const [palette, setPalette] = useContext(PaletteContext);
+function ColorPairContrastCard(props) {
+    const { colorPair } = props;
+
     return (
         <div className="card-container"> 
             <div className="card-content" style={{backgroundColor: 'white', color: 'black'}}>
                 <div className="class-body">
-                    <div className="colorPreviewer" style={{backgroundColor: palette.colors[0], color: palette.colors[1]}}>
-                        <div>Font Size: 12, Reg</div>
-                        <div>Font-Size: 12, Bold</div>
-                        <div>Font-Size: 14, Reg</div>
-                        <div>Font-Size: 14, Bold</div>
+                    <div className="colorPreviewer" style={{backgroundColor: colorPair.colorPair[0].toHexString(), color: colorPair.colorPair[1]}}>
+                        <div className="font12">Font Size: 12, Reg</div>
+                        <div className="font12 font-bold">Font-Size: 12, Bold</div>
+                        <div className="font14">Font-Size: 14, Reg</div>
+                        <div className="font14 font-bold">Font-Size: 14, Bold</div>
                     </div>
                     <div className="contrastRatings">
-                        <div>Foreground: #XXXXX</div>
-                        <div>Background: #XXXXX</div>
-                        <div>AA: "Pass/Fail"</div>
-                        <div>AA-Large: "Pass/Fail"</div>
-                        <div>AAA: "Pass/Fail"</div>
-                        <div>AAA-large: "Pass/Fail"</div> 
+                        <div>Foreground: {colorPair.colorPair[0].toHexString()}</div>
+                        <div>Background: {colorPair.colorPair[1].toHexString()}</div>
+                        <div>Ratio: {colorPair.contrastRatings.ratio}</div>
+                        <div>AA: {colorPair.contrastRatings.AA}</div>
+                        <div>AA-Large: {colorPair.contrastRatings.AALarge}</div>
+                        <div>AAA: {colorPair.contrastRatings.AAA}</div>
+                        <div>AAA-large: {colorPair.contrastRatings.AAALarge}</div> 
                     </div>
                 </div>
-            </div>  
-            <div className="card-content" style={{backgroundColor: 'white', color: 'black'}}>
-                <div className="class-body ">
-                    <div className="colorPreviewer" style={{backgroundColor: palette.colors[1], color: palette.colors[1]}}>
-                        <div>Font Size: 12, Reg</div>
-                        <div>Font-Size: 12, Bold</div>
-                        <div>Font-Size: 14, Reg</div>
-                        <div>Font-Size: 14, Bold</div>
-                    </div>
-                    <div className="contrastRatings">
-                        <div>Foreground: #XXXXX</div>
-                        <div>Background: #XXXXX</div>
-                        <div>AA: "Pass/Fail"</div>
-                        <div>AA-Large: "Pass/Fail"</div>
-                        <div>AAA: "Pass/Fail"</div>
-                        <div>AAA-large: "Pass/Fail"</div> 
-                    </div>
-                </div>
-            </div>            
-            <div className="card-content" style={{backgroundColor: 'white', color: 'black'}}>
-                <div className="class-body ">
-                    <div className="colorPreviewer" style={{backgroundColor: palette.colors[2], color: palette.colors[1]}}>
-                        <div>Font Size: 12, Reg</div>
-                        <div>Font-Size: 12, Bold</div>
-                        <div>Font-Size: 14, Reg</div>
-                        <div>Font-Size: 14, Bold</div>
-                    </div>
-                    <div className="contrastRatings">
-                        <div>Foreground: #XXXXX</div>
-                        <div>Background: #XXXXX</div>
-                        <div>AA: "Pass/Fail"</div>
-                        <div>AA-Large: "Pass/Fail"</div>
-                        <div>AAA: "Pass/Fail"</div>
-                        <div>AAA-large: "Pass/Fail"</div> 
-                    </div>
-                </div>
-            </div>            
-            <div className="card-content" style={{backgroundColor: 'white', color: 'black'}}>
-                <div className="class-body ">
-                    <div className="colorPreviewer" style={{backgroundColor: palette.colors[3], color: palette.colors[1]}}>
-                        <div>Font Size: 12, Reg</div>
-                        <div>Font-Size: 12, Bold</div>
-                        <div>Font-Size: 14, Reg</div>
-                        <div>Font-Size: 14, Bold</div>
-                    </div>
-                    <div className="contrastRatings">
-                        <div>Foreground: #XXXXX</div>
-                        <div>Background: #XXXXX</div>
-                        <div>AA: "Pass/Fail"</div>
-                        <div>AA-Large: "Pass/Fail"</div>
-                        <div>AAA: "Pass/Fail"</div>
-                        <div>AAA-large: "Pass/Fail"</div> 
-                    </div>
-                </div>
-            </div>            
-            <div className="card-content" style={{backgroundColor: 'white', color: 'black'}}>
-                <div className="class-body ">
-                    <div className="colorPreviewer" style={{backgroundColor: palette.colors[4], color: palette.colors[1]}}>
-                        <div>Font Size: 12, Reg</div>
-                        <div>Font-Size: 12, Bold</div>
-                        <div>Font-Size: 14, Reg</div>
-                        <div>Font-Size: 14, Bold</div>
-                    </div>
-                    <div className="contrastRatings">
-                        <div>Foreground: #XXXXX</div>
-                        <div>Background: #XXXXX</div>
-                        <div>AA: "Pass/Fail"</div>
-                        <div>AA-Large: "Pass/Fail"</div>
-                        <div>AAA: "Pass/Fail"</div>
-                        <div>AAA-large: "Pass/Fail"</div> 
-                    </div>
-                </div>
-            </div>                                                              
+            </div>                                                               
         </div>
-
     );
 };
 export default ColorPairContrastCard
--- src/website-color-generator-app/src/pages/ContrastChecker/ContrastChecker.js
+++ src/website-color-generator-app/src/pages/ContrastChecker/ContrastChecker.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useContext, useMemo, useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './ContrastChecker.scss';
 import ColorPairContrastCard from '../../components/ColorPairContrastCard/ColorPairContrastCard';
+import { PaletteContext } from '../../context/PaletteContext';
 
-const ContrastChecker = () => (
-  <div className="ContrastChecker" data-testid="ContrastChecker">
-    <h1> Contrast Checker</h1>
-    <ColorPairContrastCard
-      title = 'Contrast Card'
-      body='Font size 12, Reg Font-size:12'
-      />
-  </div>
- 
-);
+const ContrastChecker = () => {
+
+  const [palette, setPalette] = useContext(PaletteContext);
+  
+  useEffect(() => {
+    alert(palette.colorPairs.length);
+  }, [palette]);
+  
+  return palette.colorPairs && (
+    <div className="ContrastChecker" data-testid="ContrastChecker">
+      <h1>Contrast Checker</h1>
+      <>
+        {
+          palette.colorPairs.map((cp, key) => {
+            return (
+              <ColorPairContrastCard
+                colorPair={cp} key={key}
+              />
+            );
+          })
+        }
+      </>
+    </div>
+  );
+};
 
 ContrastChecker.propTypes = {};
 
 ContrastChecker.defaultProps = {};
 
-export default ContrastChecker;
+export default React.memo(ContrastChecker);
