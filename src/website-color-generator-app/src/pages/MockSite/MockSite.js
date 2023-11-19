8e0ae6f48ac87a0bebea0783bcf86a61c422acb5
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-11-19 15:41:30 -0500

    Replaced anchor elements with div so user cannot get redirected to a different link

--- src/website-color-generator-app/src/pages/MockSite/MockSite.js
+++ src/website-color-generator-app/src/pages/MockSite/MockSite.js
@@ -10,7 +10,7 @@ const MockSite = () => {
   //Work-around to get change colors of pseudo classes
   //https://stackoverflow.com/questions/11371550/change-hover-css-properties-with-javascript
   
-  const mockNavItems = document.querySelectorAll('.inactive a');
+  const mockNavItems = document.querySelectorAll('.inactive div');
 
   mockNavItems.forEach((navItem) => {
     navItem.style.setProperty('--nav-item-background-color', `${palette.colors[1]}`);
@@ -20,14 +20,14 @@ const MockSite = () => {
   return (
     <div className="MockSite" data-testid="MockSite" style={{ backgroundColor: palette.colors[0] }}>
     <ul className="mock-nav" style={{backgroundColor: palette.colors[4]}}>
-      <li className="mock-nav-item active" style={{backgroundColor: palette.colors[2]}}><a href="" style={{color: palette.colors[2].isLight() ? palette.colors[4] : palette.colors[0]}}>Home</a></li>
-      <li className="mock-nav-item inactive"><a href="">Link 1</a></li>
-      <li className="mock-nav-item inactive"><a href="">Link 2</a></li>
-      <li className="mock-nav-item inactive"><a href="">Link 3</a></li>
+      <li className="mock-nav-item active" style={{backgroundColor: palette.colors[2]}}><div href="" style={{color: palette.colors[2].isLight() ? palette.colors[4] : palette.colors[0]}}>Home</div></li>
+      <li className="mock-nav-item inactive"><div href="">Link 1</div></li>
+      <li className="mock-nav-item inactive"><div href="">Link 2</div></li>
+      <li className="mock-nav-item inactive"><div href="">Link 3</div></li>
     </ul> 
     <h1 style={{color: palette.colors[4]}}> Mock Site Page</h1>
      <section>
-      <h2 style={{color: palette.colors[3]}}>Title 1</h2>
+      <h2 style={{color: palette.colors[3]}}>Sub Header</h2>
       <img src= "https://m.media-amazon.com/images/I/71sgtpB-rxL.jpg" alt="Random Color Generator" width="300" height="300" style={{backgroundColor: palette.colors[0]}}/>
       <p style={{backgroundColor: palette.colors[3], color:palette.colors[3].isLight() ? palette.colors[4]: 'white'}}> Lorem ipsum dolor sit amet. Sed dolore magnam non alias sunt ut quas quia. Sit galisum sapiente et corporis blanditiis id doloribus quidem. Ut aperiam optio ab temporibus tempora est nihil accusantium in quos voluptatibus hic suscipit fuga non nihil velit non neque reiciendis. Eos quia accusantium et explicabo accusamus in expedita consequatur et aliquid magni id saepe omnis ut consectetur quam qui nostrum sint. </p>	
       <p style={{backgroundColor: palette.colors[2], color:palette.colors[2].isLight() ? palette.colors[4]: 'white'}}>Et sint excepturi ut quia quidem ut illo quae. In minus deserunt ab itaque quidem ut internos galisum est tempore repellendus. Et odit quaerat 33 fuga saepe id dolorem veniam hic deserunt ducimus qui repudiandae debitis sed quasi iste id excepturi quos. Nam tempore iusto aut voluptatem nesciunt qui omnis deleniti? In quis labore ut soluta nemo et autem iure eos velit earum. Quo atque dolor qui harum eaque in sequi nisi et voluptas atque vel illum animi. Nam consequatur quibusdam et expedita molestias eos dolore laborum eos consectetur porro!</p>
@@ -35,7 +35,7 @@ const MockSite = () => {
     </section>	
 		
   <aside style={{backgroundColor: palette.colors[1]}} >
-    <h2 style={{color: palette.colors[4]}}>Aside Title</h2>
+    <h2 style={{color: palette.colors[4]}}>Section Header</h2>
 		<img src= "https://www.gigacalculator.com/img/randomizers/color-wheel-picker.jpg" alt="Random Color Generator"/>
 		<p> Ea vero modi aut velit itaque hic iusto necessitatibus est totam dolor. Non corporis iure aut illo dolorem 33 Quis dolorum est nisi distinctio. Et vitae quas est ipsum debitis et ipsa fugit aut quibusdam eaque cum cumque sint id nihil veniam? Et minus sequi vel perferendis nulla hic nihil maxime.</p>
     <button className="button" style={{color: palette.colors[2].isLight() ? palette.colors[4] : palette.colors[0], backgroundColor: palette.colors[2]}}>Test Button</button>
