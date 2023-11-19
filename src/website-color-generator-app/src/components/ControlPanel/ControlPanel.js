162aad67ecbaa677081064389bc2a95c5b4859c9
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-11-19 16:00:28 -0500

    Commented out attempted bug fix. To save for later

--- src/website-color-generator-app/src/components/ControlPanel/ControlPanel.js
+++ src/website-color-generator-app/src/components/ControlPanel/ControlPanel.js
@@ -17,20 +17,39 @@ const ControlPanel = () => {
     <h2>Control Panel</h2>
       <PalettePreviewer palette={palette} />
       <ActionButton name='Generate Palette' propPalette={palette} propAction={
-        async () => {
-          await generate(palette)
-            .then(async (newPalette) => { await newPalette.UpdateColorPairs(); return await newPalette; })
-            .then(async (hydratedPalette) => setPalette(await hydratedPalette));
+        async () => { setPalette(await palette.GeneratePalette());
         }} />
       <ActionButton name='Adjust Palette' propPalette={palette} propAction={
-        async () => {
-          await adjust(palette)
-            .then(async (newPalette) => { await newPalette.UpdateColorPairs(); return await newPalette; })
-            .then(async (hydratedPalette) => setPalette(await hydratedPalette));
+        async () => { setPalette(await palette.AdjustPalette());
         }} />
       <ActionButton name='Copy Palette' propPalette={palette} propAction={() => { palette.CopyPalette() }} />
   </div>
   );
+
+  //ATTEMPTED BUG FIX SOLUTION
+  //---------------------------------------------------------------------------------------------------------------
+  // const generate = async (palette) => { return await palette.GeneratePalette() };
+  // const adjust = async (palette) => { return await palette.AdjustPalette() };
+
+  // return (
+  // <div className="ControlPanel" data-testid="ControlPanel">
+  //   <h2>Control Panel</h2>
+  //     <PalettePreviewer palette={palette} />
+  //     <ActionButton name='Generate Palette' propPalette={palette} propAction={
+  //       async () => {
+  //         await generate(palette)
+  //           .then(async (newPalette) => { await newPalette.UpdateColorPairs(); return await newPalette; })
+  //           .then(async (hydratedPalette) => setPalette(await hydratedPalette));
+  //       }} />
+  //     <ActionButton name='Adjust Palette' propPalette={palette} propAction={
+  //       async () => {
+  //         await adjust(palette)
+  //           .then(async (newPalette) => { await newPalette.UpdateColorPairs(); return await newPalette; })
+  //           .then(async (hydratedPalette) => setPalette(await hydratedPalette));
+  //       }} />
+  //     <ActionButton name='Copy Palette' propPalette={palette} propAction={() => { palette.CopyPalette() }} />
+  // </div>
+  // );
 
 }
 ControlPanel.propTypes = {};
