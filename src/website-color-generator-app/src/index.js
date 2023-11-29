bbb1ef73a7dfc94578046a1b9ca89a1465f4371f
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-11-29 01:36:57 -0500

    Added a meta tag to handle content security policy to handle api request to colormind API which uses an HTTP protocol

--- src/website-color-generator-app/src/index.js
+++ src/website-color-generator-app/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <App />
   </React.StrictMode>
 );
